0fb90333f375b18fcd39c905cc47f571f673252d
juan <jmflorezff@gmail.com>
2018-02-17 08:10:55 -0600

    Extracted activity superclass for common menu elements

--- Theia/app/src/main/java/arjh/utdallas/edu/theia/DestinationSelectionActivity.java
+++ Theia/app/src/main/java/arjh/utdallas/edu/theia/DestinationSelectionActivity.java
@@ -1,18 +1,15 @@
 package arjh.utdallas.edu.theia;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class DestinationSelectionActivity extends AppCompatActivity {
+import arjh.utdallas.edu.theia.basics.activity.BasicMenuActivity;
+
+public class DestinationSelectionActivity extends BasicMenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +24,5 @@ public class DestinationSelectionActivity extends AppCompatActivity {
                 new ArrayAdapter<>(this,
                         android.R.layout.simple_list_item_1,
                         Arrays.asList("Restroom", "Drinking Fountain", "Elevator", "Exit")));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.basic_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_item_where_am_i) {
-            Toast.makeText(this, getString(R.string.toast_you_are_at, "Floor 2, Room 2.801"), Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ Theia/app/src/main/java/arjh/utdallas/edu/theia/basics/activity/BasicMenuActivity.java
@@ -0,0 +1,32 @@
+package arjh.utdallas.edu.theia.basics.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import arjh.utdallas.edu.theia.R;
+
+/**
+ * Contains basic menu features that will be used across many activities.
+ */
+public abstract class BasicMenuActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.basic_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_where_am_i) {
+            Toast.makeText(this,
+                    getString(R.string.toast_you_are_at, "Floor 2, Room 2.801"),
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
