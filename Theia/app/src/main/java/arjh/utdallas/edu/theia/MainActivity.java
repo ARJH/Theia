e04643348b84b9bca66c6e4d711b6922c6837b33
juan <jmflorezff@gmail.com>
2018-02-16 21:05:20 -0600

    Implemented mockup main activity

--- Theia/app/src/main/java/arjh/utdallas/edu/theia/MainActivity.java
+++ Theia/app/src/main/java/arjh/utdallas/edu/theia/MainActivity.java
@@ -1,7 +1,8 @@
 package arjh.utdallas.edu.theia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
     }
+
 }
